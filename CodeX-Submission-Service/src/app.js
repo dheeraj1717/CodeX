52b973e68fe6027ce2372b1a9c8d2c150e59ef0b
dheeraj1717 <dheerajjangid1717@gmail.com>
2026-04-19 03:41:15 +0530

    Fix: Final CORS policy for Submission Service

--- CodeX-Submission-Service/src/app.js
+++ CodeX-Submission-Service/src/app.js
@@ -4,6 +4,9 @@ const repositoryPlugin = require("./repository/repositoryPlugin");
 async function app(fastify, options) {
     fastify.register(require("@fastify/cors"), {
         origin: "*",
+        methods: ["GET", "PUT", "POST", "DELETE", "OPTIONS"],
+        allowedHeaders: ["Content-Type", "Authorization", "set-cookie"],
+        credentials: true
     });
 
     // register test route
