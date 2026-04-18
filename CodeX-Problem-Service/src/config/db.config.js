d977084d72aff1463d0c13231ebf7305bcecb90f
dheeraj1717 <dheerajjangid1717@gmail.com>
2026-04-19 03:04:27 +0530

    Fix: Ensure database connection in both production and development

--- CodeX-Problem-Service/src/config/db.config.js
+++ CodeX-Problem-Service/src/config/db.config.js
@@ -3,10 +3,8 @@ const { ATLAS_DB_URI, NODE_ENV } = require("./server.config");
 
 async function connectDB() {
   try {
-    if (NODE_ENV == "development") {
-      await mongoose.connect(ATLAS_DB_URI);
-      console.log("Connected to database");
-    }
+    await mongoose.connect(ATLAS_DB_URI);
+    console.log("Connected to database");
   } catch (error) {
     console.log("Error connecting to database");
     throw error;
--- CodeX-Submission-Service/src/config/dbConfig.js
+++ CodeX-Submission-Service/src/config/dbConfig.js
@@ -3,10 +3,8 @@ const { ATLAS_URI, NODE_ENV } = require("./serverConfig");
 
 async function connectDB() {
   try {
-    if (NODE_ENV == "development") {
-      await mongoose.connect(ATLAS_URI);
-      console.log("Connected to database");
-    }
+    await mongoose.connect(ATLAS_URI);
+    console.log("Connected to database");
   } catch (error) {
     console.log("Error connecting to database");
     throw error;
